8754f9d670c2140057cf928217149d9a3a4b56e8
Samolodot <135865143+Samolodot@users.noreply.github.com>
2023-09-06 10:13:46 +0300

    Add files via upload

---
+++ 2pr/zd2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("*************************");
+Console.WriteLine("Банк");
+Console.WriteLine("*************************");
+double x;
+
+Console.WriteLine("Было вложено");
+x = double.Parse(Console.ReadLine());
+Console.WriteLine("**************************");
+
+x = (x + x * 3 / 100);
+Console.WriteLine($"Вклад за год 3% = {x} рублей");
+Console.WriteLine("**************************");
+
+x = (x + x * 8 / 100);
+Console.WriteLine($"Вклад за два года 8% = {x} рублей");
+Console.WriteLine("**************************");
+Console.WriteLine($"Денег на вашем счету за два года = {x} рублей");
+
+Console.ReadKey();
+
